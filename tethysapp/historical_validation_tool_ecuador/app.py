68fae6ea2c96e8e32d829d6f9e7dd60e487737ec
romer8 <gio.busrom@gmail.com>
2022-11-07 14:50:52 -0700

    index changed

--- tethysapp/historical_validation_tool_ecuador/app.py
+++ tethysapp/historical_validation_tool_ecuador/app.py
@@ -7,7 +7,7 @@ class HistoricalValidationToolEcuador(TethysAppBase):
     """
 
     name = 'Historical Validation Tool Ecuador'
-    index = 'historical_validation_tool_ecuador:home'
+    index = 'home'
     icon = 'historical_validation_tool_ecuador/images/hvt.jpg'
     package = 'historical_validation_tool_ecuador'
     root_url = 'historical-validation-tool-ecuador'
